266d027d2f3a9533e893e8bf4effde7d7c036643
Nikki Boyd <boyd.nikki@icloud.com>
2018-09-07 13:11:28 -0700

    create editpint component and style

---
+++ src/components/EditPint.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function EditPint() {
+    var button = {
+        backgroundColor: '#FFD24F',
+    }
+
+    return (
+        <div>
+            <button style={button}>Edit Details</button>
+        </div>
+    );
+}
+
+export default EditPint
