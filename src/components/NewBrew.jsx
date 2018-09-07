3480a85de05bd47fae166666f48965c34081dc00
Nikki Boyd <boyd.nikki@icloud.com>
2018-09-07 14:47:32 -0700

    set up overall form structure on add brew page

--- src/components/NewBrew.jsx
+++ src/components/NewBrew.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 
 function NewBrew(){
+
+    var form = {
+        padding: 50,
+        backgroundColor: '#333842',
+        maxWidth: 900,
+        margin: 'auto'
+    }
+
+    var title = {
+        fontSize: 30,
+        color: '#DDDDDD',
+        textAlign: 'center'
+    }
+
   return (
-<div class="stack">
-  <div class="newKeg card">
-   <h1>Add a new brew</h1>
-   <div>
-   <label>Name:</label>
-   <input placeholder="Drop Top Amber Ale"></input>
-  </div>
-  <div>
-   <label>Brand:</label>
-   <input placeholder="Widmer Brothers"></input>
-  </div>
-  <div>
-   <label>Price:</label>
-   <input placeholder="$6.00 per pint" type="number"></input>
-  </div>
-  <div>
-   <label>Alcohol Content:</label>
-   <input placeholder="5.3" type="number"></input>
-  </div>
-  <div>
-   <label>Pints Available:</label>
-   <input value="124" type="number"></input>
-  </div>
-    <button class="btn btn-dark">Add Keg</button>
-  </div>
-</div>
+    <div>
+        <div>
+            <h1 style={title}>Add a new brew</h1>
+        </div>
+        <form style={form}>
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                    <label>Name:</label>
+                    <input placeholder="Drop Top Amber Ale"></input>
+                    </div>
+                    <div className="form-group col-md-6">
+                    <label>Brand:</label>
+                    <input placeholder="Widmer Brothers"></input>
+                    </div>
+            </div>
+            <div className="form-row">
+                <div className="form-group col-md-4">
+                    <label>Price:</label><br/>
+                    <input placeholder="$6.00 per pint" type="number"></input>
+                </div>
+                <div className="form-group col-md-4">
+                    <label>Alcohol Content:</label>
+                    <input placeholder="5.3" type="number"></input>
+                </div>
+                <div className="form-group col-md-4">
+                    <label>Pints Available:</label>
+                    <input value="124" type="number"></input>
+                </div>  
+            </div>
+            <button class="btn">Add Keg</button>
+        </form>
+    </div>
   );
 }
 
