66b178c695397646135c6b80dd5d502e0d1c1fa5
Nikki Boyd <boyd.nikki@icloud.com>
2018-09-14 13:26:14 -0700

    style terms page

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Terms(props){
+
+  var notice = {
+    fontSize: 14,
+    color: '#DDDDDD',
+    textAlign: 'center',
+    maxWidth: '600px',
+    margin: 'auto',
+}
+
+var button = {
+    margin: 30
+}
+
   return (
-    <div>
-      <p>By continuing, I agree to the Terms and Conditions and Privacy Policy of Ecliptic Brewing, LLC. I confirm that I have received the proper employee training and am subject to disciplinary action if information is not inputted as instructed by my supervisor.</p>
-      <button onClick={props.onAddBrewConfirmation}>I agree</button>
+    <div style={notice}>
+      <p style={notice}>By continuing, I agree to the Terms and Conditions and Privacy Policy of Ecliptic Brewing, LLC. I confirm that I have received the proper employee training and am subject to disciplinary action if information is not inputted as instructed by my supervisor.</p>
+      <button style={button} onClick={props.onAddBrewConfirmation} className="btn btn-success">I agree!</button>
     </div>
   );
 }
